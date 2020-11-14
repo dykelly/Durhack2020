42786ebae387eaebe06e27f40441759f802ed082
james <james.w.chalk@durham.ac.uk>
2020-11-14 23:45:49 +0000

    added scrolling buttons to profile

--- PussyPatrol/Widgets/ProfileWidget.py
+++ PussyPatrol/Widgets/ProfileWidget.py
@@ -8,6 +8,24 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.uix.scrollview import ScrollView
+from kivy.core.window import Window
+from kivy.app import runTouchApp
 
-class ProfileWidget(BoxLayout):
-    pass
+class ProfileWidget(App): # boxlayout
+    def build(self):
+        layout = GridLayout(cols=1, spacing=10, size_hint_y=None)
+        # Make sure the height is such that there is something to scroll.
+        layout.bind(minimum_height=layout.setter('height'))
+        for i in range(101):
+            btn = Button(text="This is cat #" + str(i), size_hint_y=None, height=40)
+            layout.add_widget(btn)
+        root = ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
+        root.add_widget(layout)
+
+        return runTouchApp(root)
+
+if __name__ == '__main__':
+    ProfileWidget().run()
