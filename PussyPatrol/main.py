8b9517ba33bceb0fb03572f0965d3321f3fef9d6
Cass <qwerto227@gmail.com>
2020-11-14 16:26:07 +0000

    Add Basic Layout Code

--- PussyPatrol/main.py
+++ PussyPatrol/main.py
@@ -4,12 +4,16 @@ kivy.require('1.11.1')
 
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+
+
+class PussyPatrol(Widget):
+    pass
 
 
 class PussyPatrolApp(App):
-
     def build(self):
-        return Label(text='Hello world')
+        return PussyPatrol()
 
 
 if __name__ == '__main__':
