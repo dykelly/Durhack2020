0821552e504e6f7ecffcfd6b16e49f97e51f9f43
Cass <qwerto227@gmail.com>
2020-11-14 13:36:05 +0000

    Initial Skeleton

---
+++ PussyPatrol/main.py
@@ -0,0 +1,16 @@
+import kivy
+
+kivy.require('1.11.1')
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class PussyPatrolApp(App):
+
+    def build(self):
+        return Label(text='Hello world')
+
+
+if __name__ == '__main__':
+    PussyPatrolApp().run()
