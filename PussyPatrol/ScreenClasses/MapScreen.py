a76d06a381cb5e08e5d54096abe106b09f447ecb
Cass <qwerto227@gmail.com>
2020-11-14 20:52:22 +0000

    Added Basic Screen Skeletons

---
+++ PussyPatrol/ScreenClasses/MapScreen.py
@@ -0,0 +1,12 @@
+import kivy
+from kivy.uix.screenmanager import Screen
+
+kivy.require('1.11.1')
+
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+
+
+class MapScreen(Screen):
+    pass
