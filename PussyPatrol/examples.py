b53a9b3fa08c2fa9102ccaa14c21b85c8beafb83
Noah <noah.ie.knox@gmail.com>
2020-11-15 09:53:17 +0000

    fixed database with no duplicate entries, and fixed syntax for examples

--- PussyPatrol/examples.py
+++ PussyPatrol/examples.py
@@ -1,6 +1,6 @@
-import breeds_managers as bm
-import identified_managers as im
-import master_managers as mm
+import breeds_manager as bm
+import identified_manager as im
+import master_manager as mm
 import pictures_manager as pm
 import sightings_manager as sm
 import users_manager as um
@@ -11,17 +11,17 @@ bm.create_table_breeds()
 im.create_table_identified()
 pm.create_table_pictures()
 sm.create_table_sightings()
-um.create_table_users()
+um.create_users_table()
 
 
 ##Creating a user called Stacy who spots 3 cats 2 with pics and changes her username to Stacy's_mum.
 
 um.new_user("Stacy","123")
-a = sm.new_sightings(54.7732,-1.5764, 123456789,"Cat","Stacy") ##This return Aisd for ease
+a = sm.new_sighting(54.7732,-1.5764, 123456789,"Cat","Stacy") ##This return Aisd for ease
 pm.new_picture(a,"Cat.jpeg")
-b = sm.new_sightings(54.7732,-1.5764, 123456798,"Cat","Stacy")
+b = sm.new_sighting(54.7732,-1.5764, 123456798,"Cat","Stacy")
 pm.new_picture(b,"catttt.jpeg")
-sm.new_sightings(54.7732,-1.5764, 123456989,"Cat","Stacy")
+sm.new_sighting(54.7732,-1.5764, 123456989,"Cat","Stacy")
 um.update_username("Stacy","Stacys_mum","123")
 
 
@@ -32,7 +32,7 @@ cr = bm.new_breed("Cross","Cat")
 ##Creating a user called Karen who identifies one of the stacy cats as carlMarx 
 
 um.new_user("Karen","456")
-sm.new_sightings(54.7732,-1.5764, 123456790,"Cat","Karen")
+sm.new_sighting(54.7732,-1.5764, 123456790,"Cat","Karen")
 carl = im.new_identified("carlMarx", si, "Karen") ##returns animal id
 mm.sighting_identified(a,carl)
 
@@ -45,10 +45,10 @@ mm.sighting_identified(b,stonks)
 
 ##A mouse at dans house
 um.new_user("Dachode","chode")
-new_sightings(54.7732,-1.5764, 123456989,"Mouse","Dachode")
+sm.new_sighting(54.7732,-1.5764, 123456989,"Mouse","Dachode")
 
 ##stop! Look! HOT PUSS IN YOUR AREA
-hot_puss_in_your_area(54.773, -1.5764 ,diagDistKm=5,dateTime=None,dayPrevious=None)
+mm.hot_puss_in_your_area(54.773, -1.5764 ,diagDistKm=5,dateTime=None,dayPrevious=None)
 
 
 ##54.7732° N, 1.5764° W
