b737b0cbce964e0df083fbdd54195ac4df602487
Cass <qwerto227@gmail.com>
2020-11-15 00:23:44 +0000

    Added Basic Mapview

--- PussyPatrol/Widgets/MapWidget.py
+++ PussyPatrol/Widgets/MapWidget.py
@@ -1,5 +1,6 @@
 import kivy
 from kivy.uix.boxlayout import BoxLayout
+from kivy_garden.mapview import MapView
 from kivy.uix.screenmanager import Screen
 
 kivy.require('1.11.1')
@@ -10,4 +11,7 @@ from kivy.uix.widget import Widget
 
 
 class MapWidget(BoxLayout):
-    pass
+    def __init__(self, **kwargs):
+        super(MapWidget, self).__init__(**kwargs)
+        self.add_widget(MapView(zoom=11, lat=40, lon=20))
+
